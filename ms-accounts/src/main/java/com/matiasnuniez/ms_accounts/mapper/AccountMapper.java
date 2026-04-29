5e1f076d18806cc53d81eaeddf0e0ce41c8ad24c
Matias Nunez <matiasnuniez192@gmail.com>
2026-04-29 11:12:46 -0300

    feat: add AccountMapper & MovementsMapper

---
+++ ms-accounts/src/main/java/com/matiasnuniez/ms_accounts/mapper/AccountMapper.java
@@ -0,0 +1,31 @@
+package com.matiasnuniez.ms_accounts.mapper;
+import com.matiasnuniez.ms_accounts.model.Account;
+import com.matiasnuniez.ms_accounts.dto.AccountDTO;
+import com.matiasnuniez.ms_accounts.dto.AccountResponseDTO;
+
+public class AccountMapper {
+
+    public AccountResponseDTO toResponseDTO(Account account){
+        AccountResponseDTO dto = new AccountResponseDTO();
+        dto.setAccountID(account.getAccountID());
+        dto.setAccountNumber(account.getAccountNumber());
+        dto.setAccountType(account.getAccountType());
+        dto.setInitialBalance(account.getInitialBalance());
+        dto.setCurrentBalance(account.getCurrentBalance());
+        dto.setClientID(account.getClientID());
+        dto.setState(account.getState());
+        return dto;
+    } 
+
+    public Account toEntity(AccountDTO dto){
+        Account account = new Account();
+        account.setAccountNumber(dto.getAccountNumber());
+        account.setAccountType(dto.getAccountType());
+        account.setInitialBalance(dto.getInitialBalance());
+        account.setClientID(dto.getClientID());
+        account.setState(dto.getState());
+        account.setCurrentBalance(dto.getInitialBalance());
+        return account;
+    }
+
+    }
