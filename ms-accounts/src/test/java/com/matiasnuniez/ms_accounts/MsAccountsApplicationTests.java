e09555180a0a16f27918c1a942b3636bf43475de
Matias Nunez <matiasnuniez192@gmail.com>
2026-04-29 08:06:59 -0300

    chore: initial project setup

---
+++ ms-accounts/src/test/java/com/matiasnuniez/ms_accounts/MsAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matiasnuniez.ms_accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
